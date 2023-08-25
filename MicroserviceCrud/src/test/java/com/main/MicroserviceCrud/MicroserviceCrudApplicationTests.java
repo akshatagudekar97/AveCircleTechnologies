57b4398f28f7cf8a868d296aa9218cf3239f050f
akshatagudekar97 <akshatagudekar97@gmail.com>
2023-08-25 17:04:38 +0530

    go to repository

---
+++ MicroserviceCrud/src/test/java/com/main/MicroserviceCrud/MicroserviceCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.MicroserviceCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
