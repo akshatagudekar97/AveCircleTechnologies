2ce2e68a7a17f213c88ad67e99a17f0c4d340b45
akshatagudekar97 <akshatagudekar97@gmail.com>
2023-09-01 15:45:11 +0530

    done

---
+++ Microservice/src/test/java/com/main/MicroserviceCrud/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.MicroserviceCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ MicroserviceCrud-1/src/main/java/com/main/MicroserviceCrud/MicroserviceCrud1Application.java
@@ -0,0 +1,13 @@
+package com.main.MicroserviceCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCrud1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCrud1Application.class, args);
+	}
+
+}
